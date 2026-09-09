5cdf033356552677919ee3a63330a662099cf317
Chris Redpath <chris@strcmp.co.uk>
2026-09-09 12:29:49 +0100

    expoeriment 005 - wire up the sdram controller and test read and write at 20mhz

---
+++ experiments/005-sdram-read-write/Template.sv
@@ -0,0 +1,194 @@
+//============================================================================
+//
+//  This program is free software; you can redistribute it and/or modify it
+//  under the terms of the GNU General Public License as published by the Free
+//  Software Foundation; either version 2 of the License, or (at your option)
+//  any later version.
+//
+//  This program is distributed in the hope that it will be useful, but WITHOUT
+//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+//  more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with this program; if not, write to the Free Software Foundation, Inc.,
+//  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+//============================================================================
+
+module emu
+(
+	`include "sys/emu_ports.vh"
+);
+
+///////// Default values for ports not used in this core /////////
+
+assign ADC_BUS  = 'Z;
+assign USER_OUT = '1;
+assign {UART_RTS, UART_TXD, UART_DTR} = 0;
+assign {SD_SCK, SD_MOSI, SD_CS} = 'Z;
+assign {DDRAM_CLK, DDRAM_BURSTCNT, DDRAM_ADDR, DDRAM_DIN, DDRAM_BE, DDRAM_RD, DDRAM_WE} = '0;  
+
+assign VGA_SL = 0;
+assign VGA_F1 = 0;
+assign VGA_SCALER  = 0;
+assign VGA_DISABLE = 0;
+assign HDMI_FREEZE = 0;
+assign HDMI_BLACKOUT = 0;
+assign HDMI_BOB_DEINT = 0;
+
+assign AUDIO_S = 0;
+assign AUDIO_L = 0;
+assign AUDIO_R = 0;
+assign AUDIO_MIX = 0;
+
+assign LED_DISK = 0;
+assign LED_POWER = 0;
+assign BUTTONS = 0;
+
+//////////////////////////////////////////////////////////////////
+
+wire [1:0] ar = status[122:121];
+
+assign VIDEO_ARX = (!ar) ? 12'd4 : (ar - 1'd1);
+assign VIDEO_ARY = (!ar) ? 12'd3 : 12'd0;
+
+`include "build_id.v" 
+localparam CONF_STR = {
+	"Template;;",
+	"-;",
+	"O[122:121],Aspect ratio,Original,Full Screen,[ARC1],[ARC2];",
+	"O[2],TV Mode,NTSC,PAL;",
+	"O[4:3],Noise,White,Red,Green,Blue;",
+	"-;",
+	"P1,Test Page 1;",
+	"P1-;",
+	"P1-, -= Options in page 1 =-;",
+	"P1-;",
+	"P1O[5],Option 1-1,Off,On;",
+	"d0P1F1,BIN;",
+	"H0P1O[10],Option 1-2,Off,On;",
+	"-;",
+	"P2,Test Page 2;",
+	"P2-;",
+	"P2-, -= Options in page 2 =-;",
+	"P2-;",
+	"P2S0,DSK;",
+	"P2O[7:6],Option 2,1,2,3,4;",
+	"-;",
+	"-;",
+	"T[0],Reset;",
+	"R[0],Reset and close OSD;",
+	"v,0;", // [optional] config version 0-99. 
+	        // If CONF_STR options are changed in incompatible way, then change version number too,
+			  // so all options will get default values on first start.
+	"V,v",`BUILD_DATE 
+};
+
+wire forced_scandoubler;
+wire   [1:0] buttons;
+wire [127:0] status;
+wire  [10:0] ps2_key;
+
+hps_io #(.CONF_STR(CONF_STR)) hps_io
+(
+	.clk_sys(clk_sys),
+	.HPS_BUS(HPS_BUS),
+	.EXT_BUS(),
+	.gamma_bus(),
+
+	.forced_scandoubler(forced_scandoubler),
+
+	.buttons(buttons),
+	.status(status),
+	.status_menumask({status[5]}),
+	
+	.ps2_key(ps2_key)
+);
+
+///////////////////////   CLOCKS   ///////////////////////////////
+
+wire clk_sys;
+wire pll_locked;
+pll pll
+(
+	.refclk(CLK_50M),
+	.rst(0),
+	.outclk_0(clk_sys),
+	.locked(pll_locked)
+);
+
+wire reset = RESET | status[0] | buttons[1] | ~pll_locked;
+
+wire sdram_init_done;
+wire sdram_test_done;
+wire sdram_test_pass;
+wire [15:0] sdram_expected;
+wire [15:0] sdram_observed;
+wire [15:0] sdram_tested_words;
+
+assign SDRAM_CLK = clk_sys;
+sdram_rw_bist #(.SDRAM_FREQ_HZ(20_000_000), .TEST_WORDS(256)) sdram_test
+(
+	.clk(clk_sys), .reset(reset),
+	.sdram_a(SDRAM_A), .sdram_ba(SDRAM_BA), .sdram_cke(SDRAM_CKE),
+	.sdram_ncs(SDRAM_nCS), .sdram_nras(SDRAM_nRAS), .sdram_ncas(SDRAM_nCAS),
+	.sdram_nwe(SDRAM_nWE), .sdram_dqml(SDRAM_DQML), .sdram_dqmh(SDRAM_DQMH),
+	.sdram_dq(SDRAM_DQ), .init_done(sdram_init_done), .test_done(sdram_test_done),
+	.test_pass(sdram_test_pass), .expected_data(sdram_expected),
+	.observed_data(sdram_observed), .tested_words(sdram_tested_words)
+);
+
+wire [1:0] col = status[4:3];
+
+wire HBlank;
+wire HSync;
+wire VBlank;
+wire VSync;
+wire ce_pix;
+wire hvcnt_atzero;
+wire [7:0] video;
+
+// Leave H/V sync always on. This stabilizes the video output while the core
+// is in reset. This example releases the reset when H/V counters are at zero.
+reg reset_core = 1;
+always @(posedge clk_sys) begin
+	if(reset) reset_core <= 1;
+	else if(hvcnt_atzero) reset_core <= 0;
+end
+
+mycore mycore
+(
+	.clk(clk_sys),
+	.reset(reset_core),
+
+	.pal(status[2]),
+	.scandouble(forced_scandoubler),
+
+	.ce_pix(ce_pix),
+	.hvcnt_atzero(hvcnt_atzero),
+
+	.HBlank(HBlank),
+	.HSync(HSync),
+	.VBlank(VBlank),
+	.VSync(VSync),
+
+	.video(video)
+);
+
+assign CLK_VIDEO = clk_sys;
+assign CE_PIXEL = ce_pix;
+
+assign VGA_DE = ~(HBlank | VBlank);
+assign VGA_HS = HSync;
+assign VGA_VS = VSync;
+assign VGA_G  = (!col || col == 2) ? video : 8'd0;
+assign VGA_R  = (!col || col == 1) ? video : 8'd0;
+assign VGA_B  = (!col || col == 3) ? video : 8'd0;
+
+reg  [26:0] act_cnt;
+always @(posedge clk_sys) act_cnt <= act_cnt + 1'd1; 
+// Off while running, solid on for pass, flashing for failure.
+assign LED_USER = sdram_test_done ? (sdram_test_pass ? 1'b1 : act_cnt[22]) : 1'b0;
+
+endmodule
